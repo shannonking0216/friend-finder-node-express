1a7535deaadf0ef595c37390dd1d5aec158cbbf6
Shannon King <kingshannon01@gmail.com>
2019-04-01 23:35:16 -0700

    commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,515 @@
 const friendData = {
-  "name":"",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-}
+  "name": "Ahmed",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+  {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+]
+},
+{
+  "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+},
+{
+  "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+      "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+},
+{
+  "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+},
+{
+  "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+},
+{
+  "name": "Shroom Burger",
+    "photo": "https://friend-finder-fsf.herokuapp.com/",
+      "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Rebecca",
+    "photo": "jkbhjb",
+      "scores": [
+        "1",
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Fabio",
+    "photo": "https://pixel.nymag.com/imgs/fashion/daily/2016/03/17/17-fabio-citizenship.w700.h700.jpg",
+      "scores": [
+        "4",
+        "3",
+        "1",
+        "1",
+        "4",
+        "1",
+        "3",
+        "3",
+        "2",
+        "5"
+      ]
+},
+{
+  "name": "Fabio",
+    "photo": "https://pixel.nymag.com/imgs/fashion/daily/2016/03/17/17-fabio-citizenship.w700.h700.jpg",
+      "scores": [
+        "4",
+        "3",
+        "5",
+        "5",
+        "1",
+        "5",
+        "1",
+        "1",
+        "5",
+        "1"
+      ]
+},
+{
+  "name": "Fabio",
+    "photo": "https://pixel.nymag.com/imgs/fashion/daily/2016/03/17/17-fabio-citizenship.w700.h700.jpg",
+      "scores": [
+        "4",
+        "3",
+        "5",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "5",
+        "1"
+      ]
+},
+{
+  "name": "Fred",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwid96P8jK7hAhX2JTQIHd0HApkQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rd.com%2Fadvice%2Fpets%2Fdog-breed-puppy-picture-guess%2F&psig=AOvVaw00HQoJ7EB7IYnHV-s6NlNj&ust=1554180780392789",
+      "scores": [
+        "5",
+        "2",
+        "3",
+        "1",
+        "2",
+        "1",
+        "4",
+        "2",
+        "2",
+        "4"
+      ]
+},
+{
+  "name": "Fred",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwid96P8jK7hAhX2JTQIHd0HApkQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rd.com%2Fadvice%2Fpets%2Fdog-breed-puppy-picture-guess%2F&psig=AOvVaw00HQoJ7EB7IYnHV-s6NlNj&ust=1554180780392789",
+      "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+},
+{
+  "name": "Cezar Ganzon",
+    "photo": "https://i.pinimg.com/originals/12/ff/ac/12ffac01583012da82d4bec7a8988c3f.jpg",
+      "scores": [
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"
+      ]
+},
+{
+  "name": "doggo",
+    "photo": "link",
+      "scores": [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5"
+      ]
+},
+{
+  "name": "a",
+    "photo": "a",
+      "scores": [
+        "3",
+        "4",
+        "2",
+        "3",
+        "4",
+        "1",
+        "5",
+        "3",
+        "3",
+        "2"
+      ]
+},
+{
+  "name": "cvkjb",
+    "photo": "ASCDB V",
+      "scores": [
+        "1",
+        "2",
+        "3",
+        "5",
+        "4",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+},
+{
+  "name": "Samuel Thompson",
+    "photo": "nopes",
+      "scores": [
+        "2",
+        "2",
+        "3",
+        "2",
+        "1",
+        "3",
+        "3",
+        "1",
+        "3",
+        "1"
+      ]
+},
+{
+  "name": "a",
+    "photo": "a",
+      "scores": [
+        "3",
+        "3",
+        "4",
+        "4",
+        "3",
+        "3",
+        "3",
+        "4",
+        "2",
+        "2"
+      ]
+},
+{
+  "name": "Greg Rice",
+    "photo": "http://photos1.blogger.com/blogger/7631/398/1600/Alfred-3.gif",
+      "scores": [
+        "4",
+        "2",
+        "3",
+        "5",
+        "3",
+        "3",
+        "1",
+        "4",
+        "1",
+        "4"
+      ]
+},
+{
+  "name": "Taylor",
+    "photo": "https://jooinn.com/images/girl-243.jpg",
+      "scores": [
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "3",
+        "5",
+        "3",
+        "2",
+        "3"
+      ]
+},
+{
+  "name": "steve",
+    "photo": "img",
+      "scores": [
+        "3",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "2",
+        "2",
+        "3",
+        "3"
+      ]
+},
+{
+  "name": "PT",
+    "photo": "image.jpeg",
+      "scores": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+      ]
+},
+{
+  "name": "Taylor",
+    "photo": "https://jooinn.com/images/girl-243.jpg",
+      "scores": [
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "3",
+        "5",
+        "3",
+        "2",
+        "3"
+      ]
+},
+{
+  "name": "steve",
+    "photo": "img",
+      "scores": [
+        "3",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "2",
+        "2",
+        "3",
+        "3"
+      ]
+},
+{
+  "name": "Amy Gemme",
+    "photo": "www.google.com",
+      "scores": [
+        "5",
+        "5",
+        "5",
+        "1",
+        "5",
+        "3",
+        "3",
+        "5",
+        "1",
+        "4"
+      ]
+},
+{
 
-module.exports = friendData;
+  module.exports = friendData;
